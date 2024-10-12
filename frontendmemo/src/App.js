98255793eb2f6abe7fc5c6385b71b67c9cf30d59
Yosvah <eddyjosva08@gmail.com>
2024-10-12 11:41:38 +0300

    ok

--- frontendmemo/src/App.js
+++ frontendmemo/src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter,Route ,Routes } from 'react-router-dom';
 import './App.css';
-import Acceuil from './Acceuil';
+import Acceuil from './pages/Acceuil';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import '../src/Styles/bootstrap.css';
+import '../src/Styles/responsive.css';
+import '../src/Styles/style.css';
+import '../src/Styles/style.css.map';
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Acceuil />} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
