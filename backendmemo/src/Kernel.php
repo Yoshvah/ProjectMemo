30cf44edcb5b0386229892a62ac1ce2e595c28c5
Yosvah <eddyjosva08@gmail.com>
2024-10-21 14:19:44 +0300

    ok

--- backendmemo/src/Kernel.php
+++ backendmemo/src/Kernel.php
@@ -4,8 +4,15 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+}
+public function onKernelResponse(ResponseEvent $event): void {
+    $response = $event->getResponse();
+    $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
